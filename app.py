db18af5d86e9889774fa64fea5f11eadaeab64e3
“Mhapong” <125763523+Mhapong@users.noreply.github.com>
2023-06-10 13:12:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import os
 import json
-#from resnet_model import ResnetModel
+import ResnetModel
 import numpy as np
 import pandas as pd
 from fastai.vision.all import (
@@ -13,16 +13,15 @@ from fastai.vision.all import (
 from fastai.vision.all import *
 from fastai.vision.widgets import *
 import pathlib
-temp = pathlib.PosixPath
-pathlib.WindowsPath = pathlib.PosixPath #if run on window and not streamlit cloud pathlib.PosixPath = pathlib.WindowsPath
 
-model = load_learner("deploy/tytc_resnet34_fastai_R8.pkl") # load model
+c_type = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Contrails', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus']
+model = load_learner("./Cloud_resnet50_fastai.pkl") # load model
 
-st.title("Cloud _CLassy") #Title
-st.markdown('"Cloud _CLassy" is a project that will help you identify a cloud type\'s model from the image you upload.') #information
+st.title("Cloud _Classy") #Title
+st.markdown('"Cloud _CLassy" is a project that will help you identify a cloud type from the image you upload.') #information
 st.markdown("Please upload your image of cloud or use the sample images on the left sidebar.") #information
 
-sample_path = ("deploy/sample_images") #folder sameple images
+sample_path = ("./sample_images") #folder sameple images
 file_name = os.listdir(sample_path) #file name from folder
 sample_image = st.sidebar.selectbox(   #create selectbox sidebar
     'Sample images',
@@ -40,19 +39,15 @@ else:
     st.title("Here is the image you've selected") #display selected image
     st.image(img)
 
-a, b, c = model.predict(img) #predict model
+im_predicted = model.predict(img) #predict model
 
-if a in ['supra']: #easter egg
-    st.success(f"Wow! That is a **{a}**  with the probability of **{c[b]*100:.02f}**%") #result displays
+if a in c_type:
+    st.success(f"This cloud is **{a}**  with the probability of **{c[b]*100:.02f}**%") #result displays
     st.balloons()
 
 else:
-    st.success(f"The car model is **{a}**  with the probability of **{c[b]*100:.02f}**%") #result display
+    st.success(f"This cloud is **{a}**  with the probability of **{c[b]*100:.02f}**%") #result display
 
-st.markdown('For more information and info on how this app was made.')
-st.markdown('Medium : https://medium.com/@gatchanminecraft/is-that-a-supra-thai-version-405cb2231f2b')
-st.markdown('GitHub : https://github.com/BikiniGordon/Is-that-a-Supra')
-
-
-
-
+st.markdown('Link ต่างๆที่เกี่ยวข้อง')
+st.markdown('Medium : https://medium.com/@Chinochi/cloud-type-classification-cloud-class-ระบบแยกประเภทของเมฆ-6c5233f1ab8')
+st.markdown('GitHub : https://github.com/Mhapong/cloud_classify')
