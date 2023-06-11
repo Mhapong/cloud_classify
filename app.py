31d0ddce118a7b9ca8d3660ba5d74dbc41dd1f61
“Mhapong” <125763523+Mhapong@users.noreply.github.com>
2023-06-11 16:40:52 +0700

    asd

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ from fastai.vision.all import *
 import pathlib
 import urllib
 
-'''MODEL_URL = "https://github.com/Mhapong/cloud_classify/raw/main/Cloud_resnet50_fastai.pkl"
-urllib.request.urlretrieve(MODEL_URL)'''
 c_type = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Contrails', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus']
 model = load_learner('Cloud_resnet50_fastai.pkl',cpu=True) # load model
 
-st.title("Cloud _Classy") #Title
+st.title("Cloud Classification มามะมาแยกเมฆกัน") #Title
 st.markdown('"Cloud _CLassy" is a project that will help you identify a cloud type from the image you upload.') #information
 st.markdown("Please upload your image of cloud or use the sample images on the left sidebar.") #information
+st.sidebar.markdown("ถ้าขี้เกียจหรือไม่สะดวกหารูปก็เลือกข้างล่างนี้เลยยย\n\n\nV\nV\nV\nV\nV\nV\nV\nV\nV\nV")
 
 sample_path = ("./sample_images") #folder sameple images
-file_name = os.listdir(sample_path) #file name from folder
+file_name = os.listdir(sample_path)
 sample_image = st.sidebar.selectbox(   #create selectbox sidebar
     'Sample images',
     (file_name))
+
+st.sidebar.markdown("⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ‧ ⁺ ✦ ⊹ . * ꙳ ✦ ⊹ ⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ‧ ⨯. ⁺ ✦ ☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩⋆✧⭒☁️⭑⋆⭒🟉☆⭑☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡☁︎︎⋆｡˚✩☁︎︎⋆｡⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ‧ ⁺ ✦ ⊹ . * ꙳ ✦ ⊹ ⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ‧ ⨯. ⁺ ✦ ☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩✩☁︎︎⋆｡˚✩☁︎︎⋆｡☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩⋆✧⭒☁️⭑⋆⭒🟉☆⭑☁︎︎⋆｡˚⋆｡˚ ☁︎ ˚｡⋆｡˚｡⋆✩☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ✦ ⋆✧⭒☁️⭑⋆⭒🟉☆⭑⊹ . * ꙳ ✦ ⊹ ⨯ . ⁺ ✦ ⊹ ꙳ ⁺ ‧ ⨯. ⁺ ✦ ☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩☁︎︎⋆｡˚✩⋆｡˚ ☁︎ ˚｡⋆｡˚｡⋆｡˚✩☁︎︎⋆｡☁︎︎⋆｡˚✩☁︎︎⋆｡˚ ☁︎ ˚｡⋆｡˚｡⋆˚✩☁︎︎⋆｡˚✩☁︎︎")
 
 file = st.file_uploader("Upload your image") #upload file
 if file is None:
@@ -38,17 +39,17 @@ else:
     st.image(img)
 
 im_predicted = model.predict(img) #predict model
-st.write(im_predicted[0])
-st.write(im_predicted[2][6].item()*100,'%')
+c_name = im_predicted[0]
+ts_prob = im_predicted[2]
+prob = torch.sort(ts_prob, descending=True)
+m_prob = prob[0][0]
 
+if c_name in c_type:
+     st.success(f"This cloud is **{c_name}**  with the probability of **{m_prob*100:.02f}**%") #result displays
+     st.balloons()
 
-
-# if im_predicted in c_type:
-#     st.success(f"This cloud is **{a}**  with the probability of **{c[b]*100:.02f}**%") #result displays
-#     st.balloons()
-
-# else:
-#     st.success(f"This cloud is **{a}**  with the probability of **{c[b]*100:.02f}**%") #result display
+else:
+     st.success(f"Sorry, pls take another images") #result display
 
 st.markdown('Link ต่างๆที่เกี่ยวข้อง')
 st.markdown('Medium : https://medium.com/@Chinochi/cloud-type-classification-cloud-class-ระบบแยกประเภทของเมฆ-6c5233f1ab8')
